eaed764b6559fed38484c244b4b5f24d8def3f8f
Rexogamer <beartechtalks@gmail.com>
2025-01-09 14:54:37 +0000

    chore: replace chalk with picocolors

--- packages/termivolt/src/commands/help.ts
+++ packages/termivolt/src/commands/help.ts
@@ -1,5 +1,5 @@
-import chalk from "chalk";
 import os from "os";
+import picocolors from "picocolors";
 
 import { builtTimestamp, styles, version } from "../lib/index.js";
 
@@ -13,49 +13,49 @@ const help = function (debug?: boolean) {
       }`
     )}\nTermivolt is a simple utility to interact with the Revolt API via the command line.\n\n${styles.header(
       "Commands:\n"
-    )}${chalk.bold(
+    )}${picocolors.bold(
       "-send:"
-    )} Sends a message via the specified account.\n${chalk.underline(
+    )} Sends a message via the specified account.\n${picocolors.underline(
       "Example usage:"
-    )} termviolt -send <(--user/--bot)> <token> <channel ID> <message content (in quotes)> [custom API URL]\n${chalk.underline(
+    )} termviolt -send <(--user/--bot)> <token> <channel ID> <message content (in quotes)> [custom API URL]\n${picocolors.underline(
       "Notes:"
-    )} Formatting may be broken in some cases.\n\n${chalk.bold(
+    )} Formatting may be broken in some cases.\n\n${picocolors.bold(
       "-interactive:"
-    )} Use Termivolt as an interactive client.\n${chalk.underline(
+    )} Use Termivolt as an interactive client.\n${picocolors.underline(
       "Example usage:"
-    )} termviolt -interactive <(--user/--bot)> <token> [custom API URL]\n${chalk.underline(
+    )} termviolt -interactive <(--user/--bot)> <token> [custom API URL]\n${picocolors.underline(
       "Notes:"
-    )} This command does not take any extra arguments.\n\n${chalk.bold(
+    )} This command does not take any extra arguments.\n\n${picocolors.bold(
       "-fetch:"
-    )} Fetches the specified amount of messages from the specified channel.\n${chalk.underline(
+    )} Fetches the specified amount of messages from the specified channel.\n${picocolors.underline(
       "Example usage:"
-    )} termviolt -fetch <(--user/--bot)> <token> <channel ID> <message count> [custom API URL]\n${chalk.underline(
+    )} termviolt -fetch <(--user/--bot)> <token> <channel ID> <message count> [custom API URL]\n${picocolors.underline(
       "Notes:"
-    )} This requires the View Channel permission - if you get a 403 error, this might be why.\nMessages are not formatted.\n\n${chalk.bold(
+    )} This requires the View Channel permission - if you get a 403 error, this might be why.\nMessages are not formatted.\n\n${picocolors.bold(
       "-archive:"
-    )} Archives the specified channel.\n${chalk.underline(
+    )} Archives the specified channel.\n${picocolors.underline(
       "Example usage:"
-    )} termviolt -archive <(--user/--bot)> <token> <channel ID> [custom API URL]\n${chalk.underline(
+    )} termviolt -archive <(--user/--bot)> <token> <channel ID> [custom API URL]\n${picocolors.underline(
       "Notes:"
-    )} This requires the View Channel permission - if you get a 403 error, this might be why. ${chalk.bold(
+    )} This requires the View Channel permission - if you get a 403 error, this might be why. ${picocolors.bold(
       "This may take a while!"
-    )}\n\n${chalk.bold(
+    )}\n\n${picocolors.bold(
       "-kick:"
-    )} Kicks a member from the specified server.\n${chalk.underline(
+    )} Kicks a member from the specified server.\n${picocolors.underline(
       "Example usage:"
-    )} termviolt -kick <(--user/--bot)> <token> <server ID> <user ID> [custom API URL]\n${chalk.underline(
+    )} termviolt -kick <(--user/--bot)> <token> <server ID> <user ID> [custom API URL]\n${picocolors.underline(
       "Notes:"
-    )} This requires the Kick Members permission - if you get a 403 error, this might be why.\n\n${chalk.bold(
+    )} This requires the Kick Members permission - if you get a 403 error, this might be why.\n\n${picocolors.bold(
       "-rmfromgroup:"
-    )} Removes a member from the specified group.\n${chalk.underline(
+    )} Removes a member from the specified group.\n${picocolors.underline(
       "Example usage:"
-    )} termviolt -rmfromgroup <(--user/--bot)> <token> <server ID> <user ID> [custom API URL]\n${chalk.underline(
+    )} termviolt -rmfromgroup <(--user/--bot)> <token> <server ID> <user ID> [custom API URL]\n${picocolors.underline(
       "Notes:"
-    )} This requires you to own the group - if you get a 403 error, this might be why.\n\n${chalk.bold(
+    )} This requires you to own the group - if you get a 403 error, this might be why.\n\n${picocolors.bold(
       "-ban:"
-    )} Bans a member from the specified server.\n${chalk.underline(
+    )} Bans a member from the specified server.\n${picocolors.underline(
       "Example usage:"
-    )} termviolt -ban <(--user/--bot)> <token> <server ID> <user ID> [custom API URL]\n${chalk.underline(
+    )} termviolt -ban <(--user/--bot)> <token> <server ID> <user ID> [custom API URL]\n${picocolors.underline(
       "Notes:"
     )} This requires the Ban Members permission - if you get a 403 error, this might be why.`
   );
--- packages/termivolt/src/lib/constants/styles.ts
+++ packages/termivolt/src/lib/constants/styles.ts
@@ -1,11 +1,12 @@
-import chalk from "chalk";
+import picocolors from "picocolors";
 
 export default {
-  info: chalk.blue,
-  error: chalk.bold.red,
-  success: chalk.bold.green,
+  info: picocolors.blue,
+  error: (text: string) => picocolors.bold(picocolors.red(text)),
+  success: (text: string) => picocolors.bold(picocolors.green(text)),
 
   // -help styles
-  title: chalk.bold.underline.blue,
-  header: chalk.bold.underline,
+  title: (text: string) =>
+    picocolors.bold(picocolors.underline(picocolors.blue(text))),
+  header: (text: string) => picocolors.bold(picocolors.underline(text)),
 };
